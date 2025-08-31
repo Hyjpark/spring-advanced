95c3060853a0b172f230087b4314e576652b5e6c
Hyjpark <hyej042@gmail.com>
2025-09-01 00:33:10 +0900

    test: saveManager 컨트롤러 테스트 추가

---
+++ src/test/java/org/example/expert/domain/manager/controller/ManagerControllerTest.java
@@ -0,0 +1,56 @@
+package org.example.expert.domain.manager.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.expert.config.AuthUserArgumentResolver;
+import org.example.expert.config.JwtUtil;
+import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.manager.dto.request.ManagerSaveRequest;
+import org.example.expert.domain.manager.dto.response.ManagerSaveResponse;
+import org.example.expert.domain.manager.service.ManagerService;
+import org.example.expert.domain.user.dto.response.UserResponse;
+import org.example.expert.domain.user.enums.UserRole;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ManagerController.class)
+public class ManagerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private ManagerService managerService;
+
+    @MockBean
+    private AuthUserArgumentResolver authUserArgumentResolver;
+
+    @Test
+    public void Manager_저장_성공() throws Exception {
+        // given
+        long todoId = 1L;
+        ManagerSaveRequest managerSaveRequest = new ManagerSaveRequest(2L);
+        ManagerSaveResponse managerSaveResponse = ManagerSaveResponse.of(1L, UserResponse.of(2L, "qwer@qwer.com"));
+
+        given(managerService.saveManager(any(AuthUser.class), anyLong(), any(ManagerSaveRequest.class))).willReturn(managerSaveResponse);
+
+        // when * then
+        mockMvc.perform(post("/todos/{todoId}/managers", todoId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(managerSaveRequest)))
+                .andExpect(status().isOk());
+    }
+
+}
