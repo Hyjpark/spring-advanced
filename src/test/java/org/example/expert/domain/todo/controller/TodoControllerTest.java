e897313f2b5ff603f0ebd93ceffdf5b0e10c50f1
Hyjpark <hyej042@gmail.com>
2025-08-31 23:34:57 +0900

    test: getTodos 컨트롤러 테스트 추가

--- src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
+++ src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
@@ -14,13 +14,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TodoController.class)
@@ -57,5 +63,33 @@ public class TodoControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(todoSaveRequest)))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void Todo_목록을_페이지_조회_성공() throws Exception {
+        // given
+        int page = 1;
+        int size = 10;
+
+        UserResponse userResponse = UserResponse.of(1L, "asd@asd.com");
+
+        TodoResponse todo1 = TodoResponse.of(1L, "title1", "contents1", "Sunny", userResponse, LocalDateTime.now(), LocalDateTime.now());
+        TodoResponse todo2 = TodoResponse.of(2L, "title2", "contents2", "Rainy", userResponse, LocalDateTime.now(), LocalDateTime.now());
+
+        List<TodoResponse> todoList = List.of(todo1, todo2);
+        PageImpl<TodoResponse> responsePage = new PageImpl<>(todoList);
+
+        given(todoService.getTodos(page, size)).willReturn(responsePage);
+
+        // when * then
+        mockMvc.perform(get("/todos")
+                .param("page", String.valueOf(page))
+                .param("size", String.valueOf(size)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content.length()").value(todoList.size()))
+                .andExpect(jsonPath("$.content[0].id").value(todo1.getId()))
+                .andExpect(jsonPath("$.content[0].title").value(todo1.getTitle()))
+                .andExpect(jsonPath("$.content[1].id").value(todo2.getId()))
+                .andExpect(jsonPath("$.content[1].title").value(todo2.getTitle()));
     }
 }
