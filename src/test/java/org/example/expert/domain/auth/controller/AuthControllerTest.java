895f210a87c981d130a74ab369f89cddc2ff94a5
Hyjpark <hyej042@gmail.com>
2025-09-01 11:06:54 +0900

    test: signup 컨트롤러 테스트 추가

---
+++ src/test/java/org/example/expert/domain/auth/controller/AuthControllerTest.java
@@ -0,0 +1,45 @@
+package org.example.expert.domain.auth.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.expert.domain.auth.dto.request.SignupRequest;
+import org.example.expert.domain.auth.dto.response.SignupResponse;
+import org.example.expert.domain.auth.service.AuthService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AuthController.class)
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private AuthService authService;
+
+    @Test
+    public void 회원가입_성공() throws Exception {
+        // given
+        SignupRequest signupRequest = new SignupRequest("asd@asd.com", "pass", "USER");
+        SignupResponse signupResponse = SignupResponse.of("mockedToken");
+
+        given(authService.signup(any(SignupRequest.class))).willReturn(signupResponse);
+
+        // when * then
+        mockMvc.perform(post("/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signupRequest)))
+                .andExpect(status().isOk());
+    }
+}
