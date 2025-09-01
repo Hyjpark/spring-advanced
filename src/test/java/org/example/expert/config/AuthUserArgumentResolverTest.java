db89a134d741d33065cda337859121cb76fa537a
Hyjpark <hyej042@gmail.com>
2025-09-01 11:53:12 +0900

    test: AuthUserArgumentResolver의 resolveArgument 테스트 추가

--- src/test/java/org/example/expert/config/AuthUserArgumentResolverTest.java
+++ src/test/java/org/example/expert/config/AuthUserArgumentResolverTest.java
@@ -1,18 +1,20 @@
 package org.example.expert.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.example.expert.domain.auth.exception.AuthException;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.user.enums.UserRole;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.MethodParameter;
+import org.springframework.web.context.request.NativeWebRequest;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthUserArgumentResolverTest {
@@ -60,5 +62,26 @@ public class AuthUserArgumentResolverTest {
                 () -> authUserArgumentResolver.supportsParameter(parameter));
 
         assertEquals("@Auth와 AuthUser 타입은 함께 사용되어야 합니다.", exception.getMessage());
+    }
+
+    @Test
+    void resolveArgument가_Request에서_AuthUser를_꺼낸다(){
+        // given
+        NativeWebRequest webRequest = mock(NativeWebRequest.class);
+        HttpServletRequest servletRequest = mock(HttpServletRequest.class);
+        AuthUser authUser = new AuthUser(1L, "asd@asd.com", UserRole.USER);
+
+        given(webRequest.getNativeRequest()).willReturn(servletRequest);
+        given(servletRequest.getAttribute("userId")).willReturn(authUser.getId());
+        given(servletRequest.getAttribute("email")).willReturn(authUser.getEmail());
+        given(servletRequest.getAttribute("userRole")).willReturn(authUser.getUserRole().name());
+
+        // when
+        Object result = authUserArgumentResolver.resolveArgument(null, null, webRequest, null);
+
+        // then
+        assertEquals(authUser.getId(), ((AuthUser) result).getId());
+        assertEquals(authUser.getEmail(), ((AuthUser) result).getEmail());
+        assertEquals(authUser.getUserRole(), ((AuthUser) result).getUserRole());
     }
 }
